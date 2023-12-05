0f9e3b37fd0658c4ad8ace0a87d74b7c752dddc6
Adise Mamoye <adisemamo211@gmail.com>
2023-12-05 14:38:15 +0200

    connection to firebase and firestore

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="about">New Component</div>
 </template>
 
-<style>
+<script>
+import { db } from "@/firebase/connection";
+import { collection, getDocs } from "firebase/firestore";
+export default {
+  name: "AboutPage",
+  components: {},
+  props: {},
+  data: () => ({}),
+  created() {},
+  async mounted() {
+    const querySnapshot = await getDocs(collection(db, "todos"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  },
+  methods: {},
+  computed: {},
+  watch: {}
+};
+</script>
+
+<style scoped>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
