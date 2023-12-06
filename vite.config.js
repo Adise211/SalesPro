b194e8d382dd240eada827955e20dd94a14c7040
Adise Mamoye <adisemamo211@gmail.com>
2023-12-06 11:13:39 +0200

    vuetify treeshaking and sass variables

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// Vuetify Automatic treeshaking
+// Read more here: https://vuetifyjs.com/en/features/treeshaking/#automatic-treeshaking
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify({
+      styles: {
+        configFile: "src/styles/settings.scss"
+      }
+    })
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   }
-})
+});
