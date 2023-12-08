feae4590fc4ec99b01b32ef16d25bf6b83f8824d
Adise <adisemamo211@gmail.com>
2023-12-08 14:45:33 +0200

    better version of basic default layout

--- src/views/layouts/DefaultLayout.vue
+++ src/views/layouts/DefaultLayout.vue
@@ -1,33 +1,20 @@
 <template>
-  <div class="defaultLayout">
-    <!-- 1) top bar + navigation -->
-    <v-layout>
-      <v-app-bar color="white">
+  <div class="default-layout">
+    <v-layout class="rounded rounded-md">
+      <v-navigation-drawer color="grey-darken-2" v-model="isMenuOpen"></v-navigation-drawer>
+      <v-app-bar :order="order" color="grey-lighten-2" flat title="SalesPro">
+        <!-- <template v-slot:append>
+          <v-btn @click="onMenuOpen">Close menu</v-btn>
+        </template> -->
         <template v-slot:prepend>
-          <v-app-bar-nav-icon @click="isMenuOpen = !isMenuOpen"></v-app-bar-nav-icon>
+          <v-app-bar-nav-icon @click="onMenuOpen"></v-app-bar-nav-icon>
         </template>
-
-        <v-app-bar-title>SalesPro</v-app-bar-title>
-        <!-- <v-spacer></v-spacer>
-        <v-btn variant="text" icon="mdi-magnify"></v-btn> -->
       </v-app-bar>
 
-      <v-navigation-drawer v-model="isMenuOpen">
-        <v-list-item title="My Application" subtitle="Vuetify" class="mt-2"></v-list-item>
-        <v-divider></v-divider>
-        <v-list-item link title="List Item 1"></v-list-item>
-        <v-list-item link title="List Item 2"></v-list-item>
-        <v-list-item link title="List Item 3"></v-list-item>
-
-        <template v-slot:append>
-          <div class="pa-2">
-            <v-btn block> Logout </v-btn>
-          </div>
-        </template>
-      </v-navigation-drawer>
+      <v-main class="d-flex align-center justify-center" style="min-height: 300px">
+        Main Content
+      </v-main>
     </v-layout>
-    <!-- 2) main (slot) -->
-    <!-- 3) footer -->
   </div>
 </template>
 
@@ -37,11 +24,22 @@ export default {
   components: {},
   props: {},
   data: () => ({
-    isMenuOpen: false
+    isMenuOpen: false,
+    order: 0
   }),
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    onMenuOpen() {
+      if (this.order === 0) {
+        this.order = -1;
+        this.isMenuOpen = false;
+      } else {
+        this.order = 0;
+        this.isMenuOpen = true;
+      }
+    }
+  },
   computed: {},
   watch: {}
 };
