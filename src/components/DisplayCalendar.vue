fe020d7f000b6366e8f046f3d0ceb79f36b8de7e
Adise <adisemamo211@gmail.com>
2023-12-12 10:32:46 +0200

    added edit and display calendar components

---
+++ src/components/DisplayCalendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Display Calendar Comp</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data: () => ({}),
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/EditCalendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Edit Calendar Comp</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data: () => ({}),
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+};
+</script>
+
+<style scoped></style>
