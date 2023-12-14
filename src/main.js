16444d2d191b988969cbb8bd0d8c601a2c01da97
Adise <adisemamo211@gmail.com>
2023-12-14 08:16:46 +0200

    moved pinia to the plugins folder

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import pinia from "./plugins/pinia";
 import "./styles/global.scss";
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 app.use(vuetify);
 
