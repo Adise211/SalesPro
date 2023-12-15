53bb04ee055e7a1571b38c92d739f1b26c45bc6f
Adise <adisemamo211@gmail.com>
2023-12-15 08:15:29 +0200

    new layout: MinimalLayout

---
+++ src/views/layouts/MinimalLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>MinimalLayout</div>
+</template>
+
+<script>
+export default {
+  name: "MinimalLayout",
+  components: {},
+  props: {},
+  data: () => ({}),
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+};
+</script>
+
+<style scoped></style>
