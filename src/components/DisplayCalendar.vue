e014c759f5db52ae8d25fcacdf511870758ce346
Adise <adisemamo211@gmail.com>
2023-12-23 03:17:37 +0200

    next event btn

--- src/components/DisplayCalendar.vue
+++ src/components/DisplayCalendar.vue
@@ -8,13 +8,13 @@
             <div>Date</div>
             <span class="text-medium-emphasis">{{ selectedDate }}</span>
             <div class="mt-4">Company</div>
-            <span class="text-medium-emphasis">{{ currentEvents[0].Company }}</span>
+            <span class="text-medium-emphasis">{{ currentEvents[currentIndex].Company }}</span>
             <div class="mt-4">Participants</div>
-            <span class="text-medium-emphasis">{{ currentEvents[0].Participants }}</span>
+            <span class="text-medium-emphasis">{{ currentEvents[currentIndex].Participants }}</span>
           </v-col>
           <v-col>
             <div class="mt-4">Description</div>
-            <span class="text-medium-emphasis">{{ currentEvents[0].Description }}</span>
+            <span class="text-medium-emphasis">{{ currentEvents[currentIndex].Description }}</span>
           </v-col>
         </v-row>
       </div>
@@ -23,7 +23,12 @@
     <template v-slot:card-actions v-if="currentEvents.length > 0">
       <div class="pb-1 d-flex flex-row justify-space-between" style="width: 100%">
         <v-btn color="primary" class="font-weight-bold">Previous</v-btn>
-        <v-btn class="app-orange-color font-weight-bold">Next</v-btn>
+        <v-btn
+          class="app-orange-color font-weight-bold"
+          @click="onNextBtnClick"
+          :disabled="isNextBtnDisabled || currentEvents.length < 2"
+          >Next</v-btn
+        >
       </div>
     </template>
   </ViewCards>
@@ -44,11 +49,21 @@ export default {
     }
   },
   data: () => ({
-    currentIndex: -1
+    currentIndex: 0,
+    isNextBtnDisabled: false
   }),
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    onNextBtnClick() {
+      const isThelastEventInList = this.currentIndex < this.currentEvents.length;
+      if (!isThelastEventInList) {
+        this.currentIndex = this.currentIndex + 1;
+      } else {
+        this.isNextBtnDisabled = true;
+      }
+    }
+  },
   computed: {
     ...mapState(useCalendarStore, ["userEventsList"]),
     currentEvents: function () {
@@ -58,7 +73,17 @@ export default {
       return eventsList || [];
     }
   },
-  watch: {}
+  watch: {
+    selectedDate: {
+      handler(newVal, oldVal) {
+        if (newVal != oldVal) {
+          this.isNextBtnDisabled = false;
+          this.currentIndex = 0;
+        }
+      },
+      immediate: true
+    }
+  }
 };
 </script>
 
