dbebadef9afb0da7c8d9e7312740935a833ae3d5
Adise <adisemamo211@gmail.com>
2024-02-25 15:58:54 +0200

    removed old code

--- src/views/pages/OverViewPage.vue
+++ src/views/pages/OverViewPage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="calendar-page fill-height py-0">
     <v-row class="fill-height">
-      <v-col md="6" v-if="isOnEditCalendarMode">
+      <v-col md="6">
         <DisplayCalendar
           :selectedDate="selectedDate"
           @onEditEvent="
@@ -11,30 +11,10 @@
           "
         ></DisplayCalendar>
       </v-col>
-      <v-col md="6" v-if="!isOnEditCalendarMode">
-        <EditCalendar :selectedDate="selectedDate" :editEventItem="editEventItem"></EditCalendar>
-      </v-col>
+
       <v-col md="6">
         <ViewCards>
-          <template v-slot:card-text>
-            <Calendar
-              ref="vcalendar"
-              class="app-full-calendar"
-              borderless
-              transparent
-              :attributes="calendarAttrs"
-              expanded
-              @dayclick="dayClickHandler"
-            >
-              <template v-slot:footer>
-                <div class="w-25 ml-auto mr-auto">
-                  <v-btn class="w-100" color="primary" variant="flat" @click="moveToToday"
-                    >Today</v-btn
-                  >
-                </div>
-              </template>
-            </Calendar>
-          </template>
+          <template v-slot:card-text> </template>
         </ViewCards>
       </v-col>
     </v-row>
@@ -44,77 +24,21 @@
 <script>
 import ViewCards from "@/components/ViewCards.vue";
 import DisplayCalendar from "@/components/DisplayCalendar.vue";
-import EditCalendar from "@/components/EditCalendar.vue";
 import { mapState } from "pinia";
 import { useCalendarStore } from "@/stores/calendar";
-import { Calendar } from "v-calendar";
-import { CalendarPageMode } from "@/utilities/consts";
-import "v-calendar/style.css";
 
 export default {
   name: "CalendarPage",
-  components: { ViewCards, Calendar, DisplayCalendar, EditCalendar },
-  props: {
-    calendarMode: {
-      type: String,
-      default: ""
-    }
-  },
-  data: () => ({
-    isOnEditCalendarMode: true,
-    selectedDate: "",
-    editEventItem: null,
-    calendarAttrs: [
-      {
-        highlight: {
-          color: "purple",
-          fillMode: "light"
-        },
-        dates: new Date()
-      }
-    ]
-  }),
+  components: { ViewCards, DisplayCalendar },
+  props: {},
+  data: () => ({}),
   created() {},
   mounted() {},
-  methods: {
-    dayClickHandler(calendar) {
-      this.selectedDate = calendar.id;
-    },
-    moveToToday() {
-      if (this.$refs?.vcalendar) {
-        this.$refs.vcalendar.move(new Date());
-      }
-    }
-  },
+  methods: {},
   computed: {
     ...mapState(useCalendarStore, ["eventsGroupsAttr"])
   },
-  watch: {
-    calendarMode: {
-      handler(newVal) {
-        if (newVal === CalendarPageMode.View) {
-          this.isOnEditCalendarMode = false;
-        } else if (newVal === CalendarPageMode.Edit) {
-          this.isOnEditCalendarMode = true;
-        }
-      },
-      immediate: true
-    },
-    eventsGroupsAttr: {
-      handler(newData) {
-        if (newData?.length) {
-          newData.map((group) => {
-            const eventsForDisplay = {
-              dot: group.dot,
-              dates: group.dates.map((date) => new Date(date.EventDate))
-            };
-            this.calendarAttrs.push(eventsForDisplay);
-          });
-        }
-      },
-      immediate: true
-    }
-  }
+  watch: {}
 };
 </script>
 
