477a78b65438d05cf0ed95e955d998141ef02fcf
Adise <adise@wyze.co.il>
2025-02-16 11:14:20 -0500

    new page - HomePage

---
+++ src/views/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Home Page</div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  components: {},
+  props: {},
+  data: () => ({}),
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+};
+</script>
+
+<style scoped></style>
