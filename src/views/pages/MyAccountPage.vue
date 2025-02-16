2d6f682d62e65ae8b0127bec6c25efc5105469c6
Adise <adise@wyze.co.il>
2025-02-16 11:18:49 -0500

    new page - MyAccountPage

---
+++ src/views/pages/MyAccountPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>MyAccountPage</div>
+</template>
+
+<script>
+export default {
+  name: "MyAccountPage",
+  components: {},
+  props: {},
+  data: () => ({}),
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+};
+</script>
+
+<style scoped></style>
