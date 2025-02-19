14926e89d0a404bf9b5b50fe500f303ea626425c
Adise <adise@wyze.co.il>
2025-02-19 10:16:20 -0500

    added new store  - session store

---
+++ src/stores/session.js
@@ -0,0 +1,47 @@
+import { defineStore } from "pinia";
+import { Config } from "../utilities/config";
+import { convertTime } from "../utilities/utilsFuncs";
+import moment from "moment";
+
+export const useSessionStore = defineStore("session", {
+  state: () => {
+    return {
+      userId: "",
+      sessionToken: "",
+      userLastLoggedInTime: 0,
+      userFullName: "",
+      userEmail: ""
+    };
+  },
+  getters: {
+    isSessionUserActive(state) {
+      let currentTime = moment(new Date());
+      let lastLoggedInTime = moment(state.userLastLoggedInTime);
+
+      return (
+        // how much time passed since user logged in until now in hours
+        currentTime.diff(lastLoggedInTime, "hours") <
+        // the limited time user could be active in the system (from second to hours)
+        convertTime(Config.session.userActivityThreshold).hours
+      );
+    }
+  },
+  actions: {
+    setUserId(data) {
+      this.userId = data;
+    },
+    setSessionToken(token) {
+      this.sessionToken = token;
+    },
+    setUserLastLoggedInTime(time) {
+      this.userLastLoggedInTime = time;
+    },
+    setUserFullName(fullName) {
+      this.userFullName = fullName;
+    },
+    setUserEmail(email) {
+      this.userEmail = email;
+    }
+  },
+  persist: true
+});
