55572371d8bf0db9cb0bb5c646d2715559cb499a
Adise <adise@wyze.co.il>
2025-03-14 06:59:59 -0400

    products page - new table

--- src/views/pages/ProductsPage.vue
+++ src/views/pages/ProductsPage.vue
@@ -1,17 +1,91 @@
 <template>
-  <div>Products Page</div>
+  <v-container fluid>
+    <AppCard>
+      <template v-slot:card-text>
+        <v-data-table
+          :headers="tableHeaders"
+          :page="page"
+          :items-per-page="itemsPerPage"
+          :search="searchExpression"
+        >
+          <template v-slot:top>
+            <v-row class="mb-5">
+              <v-col cols="3">
+                <v-text-field
+                  v-model="searchExpression"
+                  placeholder="Search here..."
+                  append-inner-icon="mdi-magnify"
+                  hide-details
+                  single-line
+                ></v-text-field>
+              </v-col>
+              <v-col class="text-end">
+                <v-btn color="primary" variant="text" prepend-icon="mdi-plus">Add Product</v-btn>
+              </v-col>
+            </v-row>
+          </template>
+          <!-- table footer (paging) -->
+          <template v-slot:bottom>
+            <div class="text-center pt-2">
+              <v-pagination v-model="page" :length="pageCount"></v-pagination>
+            </div>
+          </template>
+        </v-data-table>
+      </template>
+    </AppCard>
+  </v-container>
 </template>
 
 <script>
+import AppCard from "@/components/AppCard.vue";
+
+const NEW_PRODUCT_OPBJECT = {
+  Name: null,
+  Description: null,
+  LastUpdated: null
+};
+
 export default {
   name: "ProductsPage",
-  components: {},
+  components: { AppCard },
   props: {},
-  data: () => ({}),
+  data: () => ({
+    page: 1,
+    itemsPerPage: "6",
+    searchExpression: "",
+    currentProduct: NEW_PRODUCT_OPBJECT
+  }),
   created() {},
   mounted() {},
   methods: {},
-  computed: {},
+  computed: {
+    tableHeaders() {
+      return [
+        {
+          title: "Index",
+          key: ""
+        },
+        {
+          title: "Name",
+          key: "Name"
+        },
+        {
+          title: "Description",
+          key: "Description"
+        },
+        {
+          title: "Last Updated",
+          key: "LastUpdated"
+        }
+      ];
+    },
+    tableItems() {
+      return [];
+    },
+    pageCount() {
+      return Math.ceil(this.tableItems.length / this.itemsPerPage);
+    }
+  },
   watch: {}
 };
 </script>
