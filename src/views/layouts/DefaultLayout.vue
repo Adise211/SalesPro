a2356c1ee52f78310349a80bbb07f533ed04cbbc
Adise <adise@wyze.co.il>
2025-03-31 15:03:48 -0400

    removed unnecessary lines

--- src/views/layouts/DefaultLayout.vue
+++ src/views/layouts/DefaultLayout.vue
@@ -62,7 +62,6 @@
 </template>
 
 <script>
-import { NavigationItems } from "@/utilities/consts";
 import { mapState } from "pinia";
 import { useSessionStore } from "../../stores/session";
 import { useGeneralStore } from "@/stores/general";
@@ -73,13 +72,13 @@ import AppFooter from "./components/AppFooter.vue";
 import VerticalNavLayout from "./components/VerticalNavLayout.vue";
 import UserMenu from "./components/UserMenu.vue";
 
+let checkUserActivityInterval;
 const USER_MENU_ACTION_TYPES = {
   Profile: "profile",
   Settings: "settings",
   Help: "help",
   Logout: "logout"
 };
-let checkUserActivityInterval;
 
 export default {
   name: "DefaultLayout",
@@ -98,51 +97,6 @@ export default {
     clearInterval(checkUserActivityInterval);
   },
   methods: {
-    onNavItemClick(itemName) {
-      let pageName;
-      let paramsObj;
-
-      switch (itemName) {
-        case NavigationItems.Home:
-          pageName = "HomePage";
-          paramsObj = {};
-          break;
-        case NavigationItems.Calendar:
-          pageName = "CalendarPage";
-          paramsObj = {};
-          break;
-        case NavigationItems.Sales:
-          pageName = "SalesPage";
-          paramsObj = {};
-          break;
-        case NavigationItems.Products:
-          pageName = "ProductsPage";
-          paramsObj = {};
-          break;
-        case NavigationItems.Customers:
-          pageName = "CustomersPage";
-          paramsObj = {};
-          break;
-        case NavigationItems.Teams:
-          pageName = "TeamsPage";
-          paramsObj = {};
-          break;
-        case NavigationItems.Files:
-          pageName = "FilesPage";
-          paramsObj = {};
-          break;
-        case NavigationItems.MyAccount:
-          pageName = "MyAccountPage";
-          paramsObj = {};
-          break;
-        default:
-          break;
-      }
-      this.$router.push({
-        name: pageName,
-        params: paramsObj
-      });
-    },
     userMenuClickHandler(action) {
       switch (action) {
         case USER_MENU_ACTION_TYPES.Profile:
@@ -191,64 +145,6 @@ export default {
       "userRole",
       "userPhotoUrl"
     ]),
-    navItems() {
-      return [
-        {
-          title: NavigationItems.Home,
-          value: NavigationItems.Home,
-          icon: "mdi-home",
-          isShownNow: false
-        },
-        // {
-        //   title: NavigationItems.Calendar,
-        //   value: NavigationItems.Calendar,
-        //   icon: "mdi-calendar",
-        //   isShownNow: false
-        // },
-        {
-          title: NavigationItems.Sales,
-          value: NavigationItems.Sales,
-          icon: "mdi-sale",
-          isShownNow: false
-        },
-        {
-          title: NavigationItems.Products,
-          value: NavigationItems.Products,
-          icon: "mdi-package-variant",
-          isShownNow: false
-        },
-        {
-          title: NavigationItems.Customers,
-          value: NavigationItems.Customers,
-          icon: "mdi-domain",
-          isShownNow: false
-        },
-        {
-          title: NavigationItems.Teams,
-          value: NavigationItems.Teams,
-          icon: "mdi-account-multiple",
-          isShownNow: false
-        },
-        // {
-        //   title: NavigationItems.Files,
-        //   value: NavigationItems.Files,
-        //   icon: "mdi-file-document",
-        //   isShownNow: false
-        // },
-        {
-          title: "My Account",
-          value: NavigationItems.MyAccount,
-          icon: "mdi-account",
-          isShownNow: false
-        }
-      ];
-    },
-    userNameDisplay() {
-      const LAST_NAME = this.userFullName.split(" ")[1];
-      const LAST_NAME_INDEX = this.userFullName.indexOf(LAST_NAME);
-
-      return this.userFullName.slice(0, LAST_NAME_INDEX + 1) + ".";
-    },
     notificationsList() {
       return [
         { id: 1, title: "test 1", subtitle: "Some text for test", date: "21/01/24", time: "12:45" },
