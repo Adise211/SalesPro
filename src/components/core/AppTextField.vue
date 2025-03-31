3415a230d3422c1573b4b3ad741c36465dd7badd
Adise <adise@wyze.co.il>
2025-03-31 06:33:15 -0400

    added new component to core components

---
+++ src/components/core/AppTextField.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-text-field
+    hide-details
+    single-line
+    density="compact"
+    variant="outlined"
+    flat
+    placeholder="Search"
+    prepend-inner-icon="mdi-magnify"
+    class="search-input inline-size"
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data: () => ({}),
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+};
+</script>
+
+<style scoped>
+.inline-size {
+  max-inline-size: 280px;
+  min-inline-size: 200px;
+}
+</style>
